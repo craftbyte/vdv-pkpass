6259deb2a0830943eb45857a64bda8dd0fbf306d
Q Misell <q@magicalcodewit.ch>
2024-12-19 10:09:14 +0100

    fix DB login script for linux

--- main/static/main/db-login-linux.sh
+++ main/static/main/db-login-linux.sh
@@ -8,13 +8,14 @@ mkdir -p ~/.local/share/applications
 cat > ~/.local/bin/db-hook <<EOF
 #!/usr/bin/env bash
 
-if [[ "$1" == "dbnav:"* ]]; then
-  url=$(echo -n $1 | base64)
-  xdg-open "https://vdv-pkpass.magicalcodewit.ch/account/db_login/callback?url=$url"
+if [[ "\$1" == "dbnav:"* ]]; then
+  url=\$(echo -n \$1 | base64)
+  xdg-open "https://vdv-pkpass.magicalcodewit.ch/account/db_login/callback?url=\$url"
 else
-  xdg-open "$1"
+  xdg-open "\$1"
 fi
 EOF
+chmod +x ~/.local/bin/db-hook
 
 cat > ~/.local/share/applications/dbnav.desktop <<EOF
 [Desktop Entry]
