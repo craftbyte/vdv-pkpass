da96576faab9148c0f098837a84f285b1ee9ae86
Q Misell <q@magicalcodewit.ch>
2024-12-19 10:09:48 +0100

    more RCT2 fuckups

--- main/uic/layout.py
+++ main/uic/layout.py
@@ -43,7 +43,8 @@ class LayoutV1:
     @classmethod
     def parse(cls, data: bytes, issuing_rics: typing.Optional[int] = None) -> "LayoutV1":
         # The Irish and the Dutch don't know how to zero index
-        if issuing_rics in (60, 1060, 1160, 84, 1084, 1184, 3268, 5188):
+        # Correction: a lot of people don't know it turns out
+        if issuing_rics in (60, 1060, 1160, 84, 1084, 1184, 3268, 5188, 9901):
             offset_x = -1
         else:
             offset_x = 0
