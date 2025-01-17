ea3ea38bd6a4db5a57b6d57767008aa6ad1ae40e
Q Misell <q@magicalcodewit.ch>
2025-01-17 22:39:08 +0100

    handle X.509 UIC cert without metadota file

--- main/uic/certs.py
+++ main/uic/certs.py
@@ -10,16 +10,21 @@ def signing_cert(rics: int, key_id: int):
     uic_storage = django.core.files.storage.storages["uic-data"]
     key_name = f"cert-{rics}_{key_id}.der"
     key_meta_name = f"cert-{rics}_{key_id}.json"
-    if uic_storage.exists(key_meta_name):
+    try:
         with uic_storage.open(key_meta_name) as key_file:
             meta = json.load(key_file)
+    except FileNotFoundError:
+        meta = None
+    try:
         with uic_storage.open(key_name) as key_file:
             try:
                 key = cryptography.x509.load_der_x509_certificate(key_file.read())
             except ValueError:
                 key = None
+    except FileNotFoundError:
+        key = None
 
-        return meta, key
+    return meta, key
 
 
 def public_key(rics: int, key_id: int):
